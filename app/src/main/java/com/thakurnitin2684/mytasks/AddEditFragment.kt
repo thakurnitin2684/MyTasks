acaa879cd6b9032111a6a6abf099279210eb8e57
Nitin Thakur <thakurnitin2684@gmail.com>
2020-05-25 13:40:38 +0530

    base code

---
+++ app/src/main/java/com/thakurnitin2684/mytasks/AddEditFragment.kt
@@ -0,0 +1,99 @@
+package com.thakurnitin2684.mytasks
+
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.app.TimePickerDialog
+import android.app.TimePickerDialog.OnTimeSetListener
+import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import kotlinx.android.synthetic.main.edit_fragment.*
+import kotlinx.android.synthetic.main.edit_fragment.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+private const val TAG="AddEditFragment"
+private const val ARG_TASK = "task"
+var time = ""
+class AddEditFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.edit_fragment, container, false)
+        val viewModel : MyTasksViewModel by activityViewModels()
+        Log.d(TAG,"onCreateView started")
+        var tID:Long=0
+        if(arguments != null) {
+            val strtTask = requireArguments().getParcelable<Task>("prevTask")
+            if (strtTask != null) {
+                root.edit_Title.editText?.text =
+                    Editable.Factory.getInstance().newEditable(strtTask.name)
+                root.edit_Description.editText?.text =
+                    Editable.Factory.getInstance().newEditable(strtTask.description)
+
+               tID=strtTask.id
+
+            }
+        }
+
+        root.edit_time.setOnClickListener{
+            showDateTimeDialog(edit_show_time)
+
+        }
+        root.edit_save.setOnClickListener{
+            val title = root.edit_Title.editText?.text.toString()
+            val description = root.edit_Description.editText?.text.toString()
+
+            val task = Task(
+                title,description , time,tID
+            )
+               viewModel.saveTask(task)
+            activity?.onBackPressed()
+        }
+        return root
+    }
+    private fun showDateTimeDialog(date_time_in: TextView) {
+        val calendar: Calendar = Calendar.getInstance()
+
+        val dateSetListener =
+            OnDateSetListener { view, year, month, dayOfMonth ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, month)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                val timeSetListener =
+                    OnTimeSetListener { view, hourOfDay, minute ->
+                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                        calendar.set(Calendar.MINUTE, minute)
+                        val simpleDateFormat = SimpleDateFormat("EEE, d MMM  HH:mm")
+                        time=simpleDateFormat.format(calendar.time)
+                        date_time_in.text= time
+                    }
+                TimePickerDialog(
+                    activity,
+                    timeSetListener,
+                    calendar.get(Calendar.HOUR_OF_DAY),
+                    calendar.get(Calendar.MINUTE),
+                    false
+                ).show()
+            }
+        DatePickerDialog(
+            requireActivity(),
+            dateSetListener,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).show()
+
+    }
+
+}
---
+++ app/src/main/java/com/thakurnitin2684/mytasks/AppDatabase.kt
@@ -0,0 +1,39 @@
+package com.thakurnitin2684.mytasks
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+private const val TAG = "AppDatabase"
+private const val DATABASE_NAME = "mytasks.db"
+private const val DATABASE_VERSION = 1
+
+internal class AppDatabase private constructor(context: Context) : SQLiteOpenHelper(
+    context, DATABASE_NAME, null,
+    DATABASE_VERSION
+) {
+    override fun onCreate(db: SQLiteDatabase) {
+        Log.d(TAG, "onCreate: starts")
+        val sSQL = """ CREATE TABLE ${TasksTable.TABLE_NAME}(
+        ${TasksTable.Columns.ID} INTEGER PRIMARY KEY NOT NULL,
+        ${TasksTable.Columns.TASK_NAME} TEXT NOT NULL,
+        ${TasksTable.Columns.TASK_DESCRIPTION} TEXT ,
+        ${TasksTable.Columns.TASK_TIME} INTEGER);""".replaceIndent(" ")
+        Log.d(TAG, sSQL)
+        db.execSQL(sSQL)
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        Log.d(TAG,"onUgrade : Starts")
+        when(oldVersion){
+            1->{
+                //on Upgrade logic
+            }
+
+        else-> throw IllegalStateException("onUpgrade() with unknown newversion: $newVersion")
+        }
+    }
+    companion object : SingletonHolder<AppDatabase,Context>(::AppDatabase)
+}
